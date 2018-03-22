74b5c4d2fd5903785a862af9347c9fb97a8a655f
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-22 14:46:24 +0200

    Created enum type Color

---
+++ SlidingTicTacToe/src/main/java/slidingttt/logic/Color.java
@@ -0,0 +1,18 @@
+package slidingttt.logic;
+
+/**
+ *
+ * @author Valhe Kouneli
+ */
+public enum Color {
+    RED,
+    BLACK;
+    
+    /**
+     * @return name of the color
+     */
+    @Override
+    public String toString() {
+        return name();
+    }
+}
