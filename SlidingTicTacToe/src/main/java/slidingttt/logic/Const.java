b4f0481c9940d447126825b5fb045030fcaf3b00
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-22 14:53:17 +0200

    made couple of static final variables get their value from how many enums of certain type there are

--- SlidingTicTacToe/src/main/java/slidingttt/logic/Const.java
+++ SlidingTicTacToe/src/main/java/slidingttt/logic/Const.java
@@ -18,8 +18,8 @@ public class Const {
     public static final int NO_COLOR = -1;
     public static final int HORIZONTAL = 0;
     public static final int VERTICAL = 1;
-    public static final int NUMBER_OF_COLORS = 2;
-    public static final int NUMBER_OF_ORIENTATIONS = 2;
+    public static final int NUMBER_OF_COLORS = Color.values().length;
+    public static final int NUMBER_OF_ORIENTATIONS = Orientation.values().length;
     public static final int DO = 0;
     public static final int UNDO = 1;
     public static final int MAX_NUMBER_ON_FIELD = 3;
