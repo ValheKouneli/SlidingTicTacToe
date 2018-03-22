e43bda575e87a3cf9ef1f2282622ee080be96fbe
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-22 19:36:43 +0200

    created humanplayer that extends Player

---
+++ SlidingTicTacToe/src/main/java/slidingttt/logic/HumanPlayer.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slidingttt.logic;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Valhe Kouneli
+ */
+public class HumanPlayer extends Player {
+
+    Scanner reader;
+    
+    public HumanPlayer(Color  color) {
+        super(color);
+    }
+    
+    public void setScanner(Scanner reader) {
+        this.reader = reader;
+    }
+    
+    @Override
+    Move getNextMove(Situation situation) {
+        Orientation orientation;
+        int index;
+        int from;
+        int dest;
+        System.out.println(situation.toString());
+        System.out.println("Give orientation.");
+        orientation = Orientation.values()[reader.nextInt()];
+        System.out.println("Give line index.");
+        index = reader.nextInt();
+        System.out.println("Give position 'from'.");
+        from = reader.nextInt();
+        System.out.println("Give destination.");
+        dest = reader.nextInt();
+                
+        return new Move(situation.getTurn(), orientation, 
+                    index, from, dest);
+    }
+    
+}
